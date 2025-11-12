f32391119c8e58ca9b61eb2a44ccf33d21243558
luizamelissa <melilux08@gmail.com>
2025-11-12 12:24:23 -0300

    Rota / Criada

---
+++ src/main/java/com/anm/estoque/controller/EstoqueController.java
@@ -0,0 +1,17 @@
+package com.anm.estoque.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class EstoqueController {
+
+    @GetMapping("/")
+    public void testeApi() {
+
+        System.out.println("A API está funcionando!");
+
+    }
+
+
+}
