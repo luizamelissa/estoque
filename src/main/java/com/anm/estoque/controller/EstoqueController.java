106d82ce6e21cf2d78ee193af147bc5574926d42
luizamelissa <melilux08@gmail.com>
2025-11-18 11:55:07 -0300

    Rota /listar criada

--- src/main/java/com/anm/estoque/controller/EstoqueController.java
+++ src/main/java/com/anm/estoque/controller/EstoqueController.java
@@ -1,10 +1,18 @@
 package com.anm.estoque.controller;
+import com.anm.estoque.service.EstoqueServico;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.anm.estoque.model.ProdutoModelo;
+
+
 @RestController
 public class EstoqueController {
+
+    @Autowired
+    private EstoqueServico es;
 
     @GetMapping("/")
     public void testeApi() {
@@ -12,6 +20,12 @@ public class EstoqueController {
         System.out.println("A API está funcionando!");
 
     }
+
+    @GetMapping("/listar")
+    public Iterable<ProdutoModelo> listar() {
+        return es.listar();
+    }
+    
 
 
 }
